00467d58ce986a3e937ba674d918483f742386fe
Srinjoy Sen Chowdhury <116475469+AllMightLegend@users.noreply.github.com>
2025-04-01 17:53:10 +0530

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCtJZTDdYUsSTEHZIZoFaidiWA4DoWEFms",
+  authDomain: "interviewai-44f9c.firebaseapp.com",
+  projectId: "interviewai-44f9c",
+  storageBucket: "interviewai-44f9c.firebasestorage.app",
+  messagingSenderId: "764002374854",
+  appId: "1:764002374854:web:71b3c9c560c6a848c7b3c7",
+  measurementId: "G-8XWNKJDEBG",
 };
 
 // Initialize Firebase
